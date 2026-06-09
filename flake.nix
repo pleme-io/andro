2e84a92cef337c1a7c34f46144fd5f0587e7f25d
drzln <luis.d@akeyless.io>
2026-06-08 17:27:23 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "andro - Android DevOps suite (CLI + daemon + MCP server)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     crate2nix.url = "github:nix-community/crate2nix";
     flake-utils.url = "github:numtide/flake-utils";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
